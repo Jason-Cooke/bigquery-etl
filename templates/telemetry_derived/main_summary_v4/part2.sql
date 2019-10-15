13b81afcdae61f75877f493a6cda3ff22b518115
Daniel Thorn <dthorn@mozilla.com>
2019-10-15 09:53:04 -0700

    Rename histogram udfs

--- templates/telemetry_derived/main_summary_v4/part2.sql
+++ templates/telemetry_derived/main_summary_v4/part2.sql
@@ -1,6 +1,6 @@
 SELECT
   document_id,
-  udf_js_histogram_row(payload, additional_properties).*,
+  udf_js_histogram_row(udf_allowed_histograms(payload, additional_properties)).*,
   udf_addon_scalars_row(payload.processes, additional_properties).*
 FROM
   `moz-fx-data-shared-prod.telemetry_stable.main_v4`
